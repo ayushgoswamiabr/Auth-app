76859673fed51e6f9a5916fe33bd64d370323e0c
ayushgoswamiabr <ayushgoswami.abr@gmail.com>
2022-01-07 22:37:15 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 5000;
-
+const path = require("path");
 //Routes
 const signuproute = require("./routes/signup");
 app.use("/api/signup", signuproute);
